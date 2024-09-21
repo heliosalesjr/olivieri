df0b9473b1bd67ec573ef4cd84b0ef4fc9897f31
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-09-21 15:13:57 -0300

    added responsivity to the footer info

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -11,18 +11,18 @@ export default function Footer() {
       <div className="max-w-5xl mx-auto flex flex-col">
         {/* Primeira Div - Container principal */}
         <div className="container mx-auto px-4">
-          <div className="flex justify-between items-start">
+          <div className="flex flex-col lg:flex-row justify-between items-center lg:items-start">
             {/* Primeira Coluna */}
-            <div className="w-2/3">
+            <div className="w-full lg:w-2/3 text-center lg:text-left">
               <p className="text-sky-500 font-black text-2xl">
                 Olivieri<span className="text-slate-200 font-semibold">Risk</span>Advisors
               </p>
-              <p className="mt-4 text-sm text-slate-300 max-w-sm">
+              <p className="mt-4 text-sm text-slate-300 max-w-sm mx-auto lg:mx-0">
                 A sua empresa especializada em seguros empresariais para todos os setores.
               </p>
 
               {/* Botões de Redes Sociais */}
-              <div className="flex space-x-4 mt-6">
+              <div className="flex justify-center lg:justify-start space-x-4 mt-6">
                 {/* E-mail */}
                 <motion.a
                   whileHover={{ scale: 1.1, backgroundColor: '#f56565' }}
@@ -51,7 +51,7 @@ export default function Footer() {
             </div>
 
             {/* Segunda Coluna */}
-            <div className="w-1/3 ml-auto">
+            <div className="hidden lg:block w-1/3 ml-auto">
               <h1 className="text-lg font-semibold mb-4">Sobre a Olivieri</h1>
               <ul className="space-y-2">
                 <li className="text-slate-300">Seguro Empresarial</li>
