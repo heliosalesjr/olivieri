0441c1b7248aa4b25ad77d66b932634eda0f2bb1
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-09-23 20:38:04 -0300

    whatzapp button animation

--- components/Whatzapp.jsx
+++ components/Whatzapp.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { FaWhatsapp } from 'react-icons/fa';
+import { motion } from 'framer-motion';
 
 export default function FloatingWhatsAppButton() {
   const whatsappNumber = "+5521986323418";
@@ -10,12 +11,20 @@ export default function FloatingWhatsAppButton() {
   };
 
   return (
-    <button
+    <motion.button
       onClick={handleClick}
-      className="fixed bottom-4 right-4 bg-green-500 rounded-full p-4 shadow-lg hover:bg-green-600 transition-colors duration-300"
+      className="fixed bottom-8 right-8 bg-green-500 rounded-full p-4 shadow-lg hover:bg-green-600 transition-colors duration-300"
       aria-label="Contact us on WhatsApp"
+      initial={{ scale: 1 }}
+      animate={{ scale: [1, 1.1, 1] }}
+      transition={{
+        duration: 1.5,
+        ease: "easeInOut",
+        repeat: Infinity,
+        repeatType: "loop"
+      }}
     >
-      <FaWhatsapp size={24} color="white" />
-    </button>
+      <FaWhatsapp size={32} color="white" />
+    </motion.button>
   );
 }
