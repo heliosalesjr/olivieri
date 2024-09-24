ed021094cb0a49b022b5ec649ea142d46a4820ad
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-09-24 16:13:54 -0300

    who we are section updated

--- components/WhoWeAre.jsx
+++ components/WhoWeAre.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { FaBolt, FaCheckCircle, FaLightbulb, FaShieldAlt } from 'react-icons/fa';
+import { FaBolt, FaCheckCircle, FaLightbulb, FaShieldAlt, FaLinkedin } from 'react-icons/fa';
 import { motion } from 'framer-motion';
 
 export default function WhoWeAre() {
@@ -10,21 +10,35 @@ export default function WhoWeAre() {
     { title: 'Inovação', description: 'Valorizamos ideias criativas.', icon: <FaLightbulb size={24} /> },
   ];
 
+  const handleLinkedInClick = () => {
+    window.open("https://www.linkedin.com/in/rosalia-olivieri", '_blank');
+  };
+
   return (
-    <div className="bg-slate-600 p-8 ">
+    <div className="bg-slate-600 p-8">
       <div>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 max-w-6xl mx-auto">
           {/* Primeira Coluna - Imagem */}
-          <motion.div className="flex justify-center items-center">
+          <motion.div className="relative flex justify-center items-center"> {/* "relative" para posicionamento absoluto dentro */}
             <motion.img
               src="/img/roi3.jpeg"
               alt="Imagem da diretora"
-              className="rounded-xl object-cover h-full w-full m-4"
+              className="rounded-xl object-cover w-full m-4"
+              style={{ maxHeight: '600px', objectFit: 'cover' }}
               initial={{ scale: 0.8, filter: 'blur(5px)' }}
               animate={{ scale: 1, filter: 'blur(0)' }}
               transition={{ duration: 1, ease: 'easeInOut' }}
               whileHover={{ scale: 0.9 }}
             />
+
+            {/* Botão com o ícone do LinkedIn */}
+            <motion.button
+              onClick={handleLinkedInClick}
+              className="absolute bottom-4 m-4 right-4 bg-sky-400 rounded-xl p-3 hover:bg-sky-300 transition-colors duration-300 shadow-lg"
+              whileHover={{ scale: 1.1 }}
+            >
+              <FaLinkedin size={32} color="white" />
+            </motion.button>
           </motion.div>
 
           {/* Segunda Coluna - Texto */}
@@ -34,10 +48,6 @@ export default function WhoWeAre() {
             <p className='text-sm text-white font-bold py-4'>Diretora Executiva de Operações</p>
             <p className="text-white mb-6">Rosália Olivieri é uma das referências no mercado de seguros, com mais de 25 anos de experiência consolidada em todo o território nacional. Apaixonada pelo que faz, ela combina conhecimento técnico com uma visão inovadora, sempre focada em entregar soluções personalizadas e eficientes. Sua liderança inspira confiança e traz resultados, tanto para a empresa quanto para seus clientes.</p>
           </div>
-
-          
-            
-          
         </div>
       </div>
     </div>
