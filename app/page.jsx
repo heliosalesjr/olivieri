ab3653a7df95b6bec9ea477b0902db9cc1029165
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-10-07 15:48:37 -0300

    responsiveness

--- app/page.jsx
+++ app/page.jsx
@@ -18,7 +18,7 @@ export default function Home() {
     <>
      
       <NewNavbar />
-      <HeroSection />
+      
       <AnotherHero  />
      
       <ServicesAlt />
--- components/AnotherHero.jsx
+++ components/AnotherHero.jsx
@@ -2,70 +2,45 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import dynamic from 'next/dynamic'
-
-// Importa Framer Motion dinamicamente, garantindo que seja carregado apenas no cliente
-const MotionDiv = dynamic(() => import('framer-motion').then(mod => mod.motion.div), { ssr: false })
 
 import heroImage from '../public/img/placeholder2.jpg'
-import Stars from '../public/img/stars.png'
 
 const AnotherHero = () => {
   return (
-    <div className="overflow-hidden bg-gray-200 py-20">
-      <div className="container mx-auto px-6 lg:flex lg:items-center lg:justify-between">
+    <div className="overflow-hidden bg-slate-200 h-[80vh] flex items-center pt-8 sm:pt-16 pb-8 sm:pb-16">
+      <div className="container mx-auto px-6 max-w-5xl lg:flex lg:items-center lg:justify-between">
         
         {/* Texto e Botões */}
-        <div className="lg:w-5/12">
-          <MotionDiv
-            initial={{ opacity: 0, y: 50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.6 }}
-          >
-            <h1 className="text-4xl sm:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-green-600">
-              Level Up Your Business
-            </h1>
-            <p className="mt-6 text-base text-gray-600 max-w-md">
-              We provide solutions to elevate your business to the next level. Let’s get started!
-            </p>
-          </MotionDiv>
+        <div className="lg:w-5/12 ">
+          <h1 className=" mt-32 md:mt-0 text-4xl sm:text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-green-600">
+            Preparados para crescer?
+          </h1>
+          <p className="mt-6 text-base text-gray-800 max-w-md">
+            Nós vamos te ajudar a alcançar o próximo nível de crescimento da sua empresa.
+          </p>
 
-          <MotionDiv
-            initial={{ opacity: 0, x: -100 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.6, delay: 0.2 }}
-            className="mt-8 flex space-x-4"
-          >
+          <div className="mt-8 flex space-x-4">
             <Link href="/start" className="px-8 py-4 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
-              Get Started
+              Saiba mais
             </Link>
             <Link href="/learn-more" className="px-8 py-4 bg-gray-100 text-gray-700 rounded-lg hover:bg-gray-200">
-              Learn More
+              Contato
             </Link>
-          </MotionDiv>
+          </div>
         </div>
 
         {/* Imagem do Hero */}
-        <MotionDiv
-          initial={{ opacity: 0, x: 100 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.6, delay: 0.4 }}
-          className="mt-12 lg:mt-0 lg:w-6/12 lg:text-right"
-        >
-          <div className="relative">
-            <Image
-              src={heroImage}
-              alt="Hero Image"
-              className="rounded-lg"
-              placeholder="blur"
-            />
-            <Image
-              src={Stars}
-              alt="Stars"
-              className="absolute top-0 right-0 w-24 opacity-70 hover:opacity-100"
-            />
+        <div className="mt-12 lg:mt-0 lg:w-6/12 lg:text-right mb-16 md:mb-0">
+          <div className="relative w-full h-full flex justify-center items-center">
+            <div className="w-[500px] aspect-square overflow-hidden rounded-tl-[20%] rounded-br-[20%] rounded-bl-[20%] rounded-tr-none">
+              <Image
+                src={heroImage}
+                alt="Hero Image"
+                className="object-cover w-full h-full"
+              />
+            </div>
           </div>
-        </MotionDiv>
+        </div>
       </div>
     </div>
   )
