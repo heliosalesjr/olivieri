4ae553e8465d33e2afe26f32f04190ab6cf56d01
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-12-09 15:51:09 -0300

    who we are component updated

--- app/page.jsx
+++ app/page.jsx
@@ -26,7 +26,7 @@ export default function Home() {
       <Services2 />
       
       <NewWhoWeAre />
-      <WhoWeAre />
+     
       <Believe />
       <NewPartners />
       <Partners />
--- components/NewWhoWeAre.jsx
+++ components/NewWhoWeAre.jsx
@@ -8,16 +8,30 @@ import solarImage from "../public/img/roi3.jpeg";
 const NewWhoWeAre = () => {
   return (
     <div className="flex flex-col lg:flex-row items-center lg:items-stretch max-w-5xl mx-auto min-h-[80vh] py-20 px-6 gap-12">
+      {/* Coluna Direita */}
+      <div className="lg:w-1/2 w-full flex justify-center items-center">
+        <div className="relative w-full h-[500px] lg:h-full rounded-2xl overflow-hidden md:max-w-[400px] sm:max-w-[400px]">
+          <Image
+            src={solarImage}
+            alt="Man holding solar panel"
+            layout="fill"
+            objectFit="cover"
+            className="rounded-2xl"
+          />
+        </div>
+      </div>
+
       {/* Coluna Esquerda */}
       <div className="lg:w-1/2 space-y-8 flex flex-col justify-center">
         <h2 className="text-3xl font-semibold text-gray-800">
         <span className="font-black">Rosália Olivieri </span>
+        <span className="text-sky-700 font-bold">|</span>
           <span className="text-gray-500"> Diretora Executiva de Operações</span>
         </h2>
         
         {/* Botão com espaçamento responsivo */}
         <div className="space-y-8 lg:space-y-20">
-          <button className="flex items-center space-x-2 px-6 py-3 bg-green-500 text-white rounded-full hover:bg-green-600">
+          <button className="flex items-center space-x-2 px-6 py-3 bg-green-500 text-white rounded-lg hover:bg-green-600">
           <FaLinkedin />
             <span>Conecte-se</span>
             
@@ -26,7 +40,7 @@ const NewWhoWeAre = () => {
           {/* Cards com espaçamento menor em mobile */}
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
             {[
-              { number: "25+", title: "Mais de 25 anos de mercado." },
+              { number: "25+", title: "Mais de 25 anos de experiência no mercado nacional." },
               { number: "50+", title: "Soluções personalizadas entregues em 2024" },
               { number: "500+", title: "Clientes atendidos em todo o Brasil." },
               { number: "100%", title: "Focada em soluções personalizadas." },
@@ -41,19 +55,6 @@ const NewWhoWeAre = () => {
               </div>
             ))}
           </div>
-        </div>
-      </div>
-
-      {/* Coluna Direita */}
-      <div className="lg:w-1/2 w-full flex justify-center items-center">
-        <div className="relative w-full h-[500px] lg:h-full rounded-2xl overflow-hidden md:max-w-[400px] sm:max-w-[400px]">
-          <Image
-            src={solarImage}
-            alt="Man holding solar panel"
-            layout="fill"
-            objectFit="cover"
-            className="rounded-2xl"
-          />
         </div>
       </div>
     </div>
