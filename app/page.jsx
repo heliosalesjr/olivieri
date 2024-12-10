1b7f11b3d4340bdffb33851bfdbdfdc38487271b
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-12-10 14:50:33 -0300

    partners component updated

--- app/page.jsx
+++ app/page.jsx
@@ -29,7 +29,7 @@ export default function Home() {
      
       <Believe />
       <NewPartners />
-      <Partners />
+     
       
       <Contact />
       <Footer />
--- components/NewPartners.jsx
+++ components/NewPartners.jsx
@@ -4,11 +4,11 @@ import { FaApple, FaGoogle, FaAmazon, FaMicrosoft, FaFacebook, FaTwitter } from 
 function NewPartners() {
   return (
     <section className=" dark:bg-gray-900 bg-slate-100">
-      <div className="py-8 lg:py-16 mx-auto max-w-screen-xl px-4">
+      <div className="lg:py-40 mx-auto max-w-screen-xl px-24 py-24">
         <h2 className="mb-4 text-3xl font-extrabold tracking-tight leading-tight text-center text-gray-900 dark:text-white md:text-4xl">
           Estamos bem acompanhados
         </h2>
-        <p className='mb-8 lg:mb-16 text-3xl font-semibold tracking-tight leading-tight text-center text-sky-500 dark:text-white md:text-2xl'>Esses são nossos parceiros</p>
+        <p className='mb-8 lg:mb-16 text-3xl font-thin tracking-tight leading-tight text-center text-sky-700 dark:text-white md:text-4xl'>Esses são nossos parceiros</p>
         <div className="grid grid-cols-2 gap-8 text-slate-800 sm:gap-12 md:grid-cols-3 lg:grid-cols-6 dark:text-gray-400">
           <a href="#" className="flex justify-center items-center">
             <FaApple size={48} />
